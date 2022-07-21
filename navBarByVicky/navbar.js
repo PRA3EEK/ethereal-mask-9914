e173cdc83abb50477f2295775375f8ef688b1818
itsvickyanand <vickyanandtoprock@gmail.com>
2022-07-21 23:46:34 +0530

    workedUponNavbarAndMainPage

---
+++ navBarByVicky/navbar.js
@@ -0,0 +1,16 @@
+let navbar = () => {
+    return `<div><a href="">Products</a></div>
+    <div><a href="">Resources</a></div>
+    <div><a href="">Inspiration</a></div>
+    <div><a href="">Pricing</a></div>
+    <div>
+        <img src="" alt="" />
+    </div>
+    <div>sales</div>
+    <div>globe</div>
+    <div>search</div>
+    <div><button>Login</button></div>
+    <div><button>Sign Up</button></div>`
+}
+
+export default navbar
