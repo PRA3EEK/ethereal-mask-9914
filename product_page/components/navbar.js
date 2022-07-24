ee5e1d6086c141e736bbea03ecce762031cdf4ab
Abhijeet Ojha <avie.just@gmail.com>
2022-07-24 14:23:35 +0530

    linking all pages

---
+++ product_page/components/navbar.js
@@ -0,0 +1,129 @@
+let func = () => {
+    document.getElementById("bdropdown").style.animation = ""
+    document.getElementById("cdropdown").style.animation = ""
+    document.getElementById("ddropdown").style.animation = ""
+    document.getElementById("edropdown").style.animation = ""
+    document.getElementById("fdropdown").style.animation = ""
+    document.getElementById("gdropdown").style.animation = ""
+    document.getElementById("adropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("dropdown").style.animation = "fadein 0.5s forwards"
+
+}
+let func1 = () => {
+    document.getElementById("adropdown").style.animation = ""
+    document.getElementById("dropdown").style.animation = ""
+}
+let func3 = () => {
+    document.getElementById("adropdown").style.animation = ""
+    document.getElementById("cdropdown").style.animation = ""
+    document.getElementById("ddropdown").style.animation = ""
+    document.getElementById("edropdown").style.animation = ""
+    document.getElementById("fdropdown").style.animation = ""
+    document.getElementById("gdropdown").style.animation = ""
+    document.getElementById("bdropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("dropdown").style.animation = "fadein 0.5s forwards"
+}
+let func4 = () => {
+    document.getElementById("bdropdown").style.animation = ""
+    document.getElementById("dropdown").style.animation = ""
+}
+let func5 = () => {
+    document.getElementById("adropdown").style.animation = ""
+    document.getElementById("bdropdown").style.animation = ""
+    document.getElementById("ddropdown").style.animation = ""
+    document.getElementById("edropdown").style.animation = ""
+    document.getElementById("fdropdown").style.animation = ""
+    document.getElementById("gdropdown").style.animation = ""
+    document.getElementById("cdropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("dropdown").style.animation = "fadein 0.5s forwards"
+}
+let func6 = () => {
+    document.getElementById("cdropdown").style.animation = ""
+    document.getElementById("dropdown").style.animation = ""
+}
+let func7 = () => {
+    document.getElementById("adropdown").style.animation = ""
+    document.getElementById("bdropdown").style.animation = ""
+    document.getElementById("cdropdown").style.animation = ""
+    document.getElementById("edropdown").style.animation = ""
+    document.getElementById("fdropdown").style.animation = ""
+    document.getElementById("gdropdown").style.animation = ""
+    document.getElementById("ddropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("dropdown").style.animation = "fadein 0.5s forwards"
+}
+let func8 = () => {
+    document.getElementById("ddropdown").style.animation = ""
+    document.getElementById("dropdown").style.animation = ""
+}
+let func9 = () => {
+    document.getElementById("adropdown").style.animation = ""
+    document.getElementById("bdropdown").style.animation = ""
+    document.getElementById("cdropdown").style.animation = ""
+    document.getElementById("ddropdown").style.animation = ""
+    document.getElementById("fdropdown").style.animation = ""
+    document.getElementById("gdropdown").style.animation = ""
+    document.getElementById("edropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("dropdown").style.animation = "fadein 0.5s forwards"
+}
+let func10 = () => {
+    document.getElementById("edropdown").style.animation = ""
+    document.getElementById("dropdown").style.animation = ""
+}
+let func11 = () => {
+    document.getElementById("adropdown").style.animation = ""
+    document.getElementById("bdropdown").style.animation = ""
+    document.getElementById("cdropdown").style.animation = ""
+    document.getElementById("ddropdown").style.animation = ""
+    document.getElementById("edropdown").style.animation = ""
+    document.getElementById("gdropdown").style.animation = ""
+    document.getElementById("fdropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("dropdown").style.animation = "fadein 0.5s forwards"
+}
+let func12 = () => {
+    document.getElementById("fdropdown").style.animation = ""
+    document.getElementById("dropdown").style.animation = ""
+}
+let func13 = () => {
+    document.getElementById("adropdown").style.animation = ""
+    document.getElementById("bdropdown").style.animation = ""
+    document.getElementById("cdropdown").style.animation = ""
+    document.getElementById("ddropdown").style.animation = ""
+    document.getElementById("edropdown").style.animation = ""
+    document.getElementById("fdropdown").style.animation = ""
+    document.getElementById("gdropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("dropdown").style.animation = "fadein 0.5s forwards"
+}
+let func14 = () => {
+    document.getElementById("gdropdown").style.animation = ""
+    document.getElementById("dropdown").style.animation = ""
+}
+let func15 = () => {
+    document.getElementById("idropdown").style.animation = ""
+    document.getElementById("jdropdown").style.animation = ""
+    document.getElementById("hdropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("IIdropdown").style.animation = "fadein 0.5s forwards"
+}
+let func16 = () => {
+    document.getElementById("hdropdown").style.animation = ""
+    document.getElementById("IIdropdown").style.animation = ""
+}
+let func17 = () => {
+    document.getElementById("hdropdown").style.animation = ""
+    document.getElementById("jdropdown").style.animation = ""
+    document.getElementById("idropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("IIdropdown").style.animation = "fadein 0.5s forwards"
+}
+let func18 = () => {
+    document.getElementById("idropdown").style.animation = ""
+    document.getElementById("IIdropdown").style.animation = ""
+}
+let func19 = () => {
+    document.getElementById("hdropdown").style.animation = ""
+    document.getElementById("idropdown").style.animation = ""
+    document.getElementById("jdropdown").style.animation = "fadein 0.5s forwards"
+    document.getElementById("IIdropdown").style.animation = "fadein 0.5s forwards"
+}
+let func20 = () => {
+    document.getElementById("jdropdown").style.animation = ""
+    document.getElementById("IIdropdown").style.animation = ""
+}
